00654ba05a03519a3433896bb9b46f04bc6f9675
Inna Ianko <inna.i@mate.academy>
2025-03-04 12:11:49 +0200

    Task added

---
+++ tests/_fixtures/fixturesGeneric.ts
@@ -0,0 +1,51 @@
+import { test as base } from '@playwright/test';
+import { Logger } from '../../src/common/logger/Logger';
+import * as allure from 'allure-js-commons';
+import { parseTestTreeHierarchy } from '../../src/common/helpers/allureHelpers';
+
+export const test = base.extend<{
+  logger;
+  infoTestLog;
+  addAllureTestHierarchy;
+}>({
+  logger: [
+    async ({}, use) => {
+      const logger = new Logger('error');
+
+      await use(logger);
+    },
+    { scope: 'worker' },
+  ],
+  infoTestLog: [
+    async ({ logger }, use, testInfo) => {
+      const indexOfTestSubfolderStart = testInfo.file.indexOf('/tests') + 7;
+      const fileName = testInfo.file.substring(indexOfTestSubfolderStart);
+
+      logger.info(`Test started: ${fileName}`);
+
+      await use('infoTestLog');
+
+      logger.info(`Test completed: ${fileName}`);
+    },
+    { scope: 'test', auto: true },
+  ],
+  addAllureTestHierarchy: [
+    async ({ logger }, use, testInfo) => {
+      const fileName = testInfo.file;
+
+      const [parentSuite, suite, subSuite] = parseTestTreeHierarchy(
+        fileName,
+        logger,
+      );
+
+      await allure.parentSuite(parentSuite);
+      await allure.suite(suite);
+      if (subSuite) {
+        await allure.subSuite(subSuite);
+      }
+
+      await use('addAllureTestHierarhy');
+    },
+    { scope: 'test', auto: true },
+  ],
+});
